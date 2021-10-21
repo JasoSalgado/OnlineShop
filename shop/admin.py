b4a129f76d78a2a19eb1e606b2fb516cbc0e9409
Jorge Salgado <jaso_98@hotmail.com>
2021-10-21 12:17:41 -0500

    ADD: shop app

---
+++ shop/admin.py
@@ -0,0 +1,25 @@
+# myshop/shop/admin.py
+# Django modules
+from django.contrib import admin
+
+# My modules
+from shop.models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Category admin
+    """
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Product admin
+    """
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    
